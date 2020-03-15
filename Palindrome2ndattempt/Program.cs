77f92d1f17d6479f582f4a378148d750ad86510c
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 19:49:49 -0500

    message

---
+++ Palindrome2ndattempt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome2ndattempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string myString = Console.ReadLine();
+            Console.WriteLine( myString.SequenceEqual(myString.Reverse()));
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
